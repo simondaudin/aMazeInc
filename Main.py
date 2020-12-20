ecef63472a609c5db2f567868895af2527e8b2f8
Simon Daudin <sdaudin@liveaction.com>
2020-12-20 18:27:35 +0100

    feat(maze): Added Maze random generation

--- Main.py
+++ Main.py
@@ -1,4 +1,67 @@
+import random
+from enum import Enum
+
 from Maze import Maze
 
-maze = Maze(2, 2, [[1, 0]], [[0, 0]])
+
+class Direction(Enum):
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
+
+
+def possible_cases(height, width, seen, i, j):
+    res = []
+    if i > 0:
+        res.append(Direction.UP)
+    if i < height - 1:
+        res.append(Direction.DOWN)
+    if j > 0:
+        res.append(Direction.LEFT)
+    if j < width - 1:
+        res.append(Direction.RIGHT)
+    return list(filter(lambda x: (i + x.value[0], j + x.value[1]) not in seen, res))
+
+
+def resolve_pos(i, j, direction):
+    return i + direction.value[0], j + direction.value[1]
+
+
+def random_maze(height, width):
+    i, j = 0, 0
+    h_walls = [[True] * width for _ in range(height - 1)]
+    v_walls = [[True] * height for _ in range(width - 1)]
+    seen = set()
+    backtrace = []
+    while True:
+        # print(Maze(height, width, h_walls, v_walls).print_with_post(i,j))
+        seen.add((i, j))
+        cases = possible_cases(height, width, seen, i, j)
+        if not cases:
+            if not backtrace:
+                return Maze(height, width, h_walls, v_walls)
+            i, j = backtrace.pop()
+            continue
+        nb_opening = random.randint(1, len(cases))
+        choices = random.sample(cases, k=nb_opening)
+        for choice in choices:
+            if choice == Direction.LEFT:
+                v_walls[j - 1][i] = False
+            elif choice == Direction.RIGHT:
+                v_walls[j][i] = False
+            elif choice == Direction.UP:
+                h_walls[i - 1][j] = False
+            elif choice == Direction.DOWN:
+                h_walls[i][j] = False
+        next_direction = random.choice(choices)
+        choices.remove(next_direction)
+        for choice in choices:
+            backtrace_pos = resolve_pos(i, j, choice)
+            seen.add(backtrace_pos)
+            backtrace.append(backtrace_pos)
+        i, j = resolve_pos(i, j, next_direction)
+
+
+maze = random_maze(20, 20)
 print(maze)
--- Maze.py
+++ Maze.py
@@ -6,14 +6,25 @@ class Maze:
         self.h_walls = h_walls
         self.v_walls = v_walls
 
-    def __repr__(self):
-        s = '+' + (Maze.wall_char(True, True)+'+') * self.width + '\n'
+    def representation(self):
+        res = [list('+' + (Maze.wall_char(True, True) + '+') * self.width)]
         for i in range(self.height - 1):
-            s += '|' + ''.join(['  '+Maze.wall_char(False, self.v_walls[j][i]) for j in range(self.width-1)]) + '  |\n'
-            s += '+' + ''.join([Maze.wall_char(True, self.h_walls[i][j])+'+' for j in range(self.width)]) + '\n'
-        s += '|' + ''.join(['  '+Maze.wall_char(False, self.v_walls[j][-1]) for j in range(self.width-1)]) + '  |\n'
-        s += '+' + (Maze.wall_char(True, True)+'+') * self.width
-        return s
+            res.append(list('|' + ''.join(
+                ['  ' + Maze.wall_char(False, self.v_walls[j][i]) for j in range(self.width - 1)]) + '  |'))
+            res.append(list('+' + ''.join([Maze.wall_char(True, self.h_walls[i][j]) + '+' for j in range(self.width)])))
+        res.append(list(
+            '|' + ''.join(['  ' + Maze.wall_char(False, self.v_walls[j][-1]) for j in range(self.width - 1)]) + '  |'))
+        res.append(list('+' + (Maze.wall_char(True, True) + '+') * self.width))
+        return res
+
+    def print_with_post(self, pos_x, pos_y):
+        r = self.representation()
+        if 0 < pos_x < self.height and 0 < pos_y < self.width:
+            r[2 * pos_x + 1][3 * pos_y + 1] = 'X'
+        return '\n'.join([''.join(line) for line in r])
+
+    def __repr__(self):
+        return self.print_with_post(-1, -1)
 
     @staticmethod
     def wall_char(horizontal, full):
